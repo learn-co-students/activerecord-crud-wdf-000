558b3f629aaed3dd7aa08a67dd3274caf68c7d74
aehmt <calisahmet@gmail.com>
2016-07-28 12:16:47 -0400

    Done.

---
+++ db/migrate/20160727192552_change_my_table_kk.rb
@@ -0,0 +1,5 @@
+class ChangeMyTableKk < ActiveRecord::Migration
+  def change
+    change_column(:movies, :release_date, :integer)
+  end
+end
---
+++ db/migrate/20160727193251_change_to_boolean.rb
@@ -0,0 +1,5 @@
+class ChangeToBoolean < ActiveRecord::Migration
+  def change
+    change_column :movies, :in_theaters, :boolean
+  end
+end
